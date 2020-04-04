8cdf35c07ddd0e9c37270a9024b9eb3e04816fbd
webzoDELP <tolo1980@gmail.com>
2020-04-04 20:05:27 +0200

    Dodaj pliki projektów.

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+
+            Console.WriteLine("Witaj, " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
+
+        }
+    }
+}
